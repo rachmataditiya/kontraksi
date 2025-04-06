fe437eb6299951e0d7d4111e1f15b4cb11029786
Rachmat Aditiya <raditiya@me.com>
2025-04-06 08:18:09 +0700

    [ADD] capacitor

---
+++ android/app/src/main/java/com/kontraksi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kontraksi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kontraksi.app',
+  appName: 'Timer Kontraksi',
+  webDir: 'dist'
+};
+
+export default config;
